d5407aaf341cb5c91365db9be26d07e2b66af5ac
MaFeLP <60669873+MaFeLP@users.noreply.github.com>
2023-04-03 10:24:16 -0500

    🐞 Fixed tests
    They are now only deserializing, as the route lines change depending on
    when they are accessed.
    
    Signed-off-by: MaFeLP <60669873+MaFeLP@users.noreply.github.com>

--- transit-api-client/src/endpoints/variants.rs
+++ transit-api-client/src/endpoints/variants.rs
@@ -119,46 +119,8 @@ mod test {
         let client = crate::TransitClient::new(
             std::env::var("WPG_TRANSIT_API_KEY").unwrap_or(String::from("")),
         );
-        let actual = rt
-            .block_on(client.variants_by_stop(50254, Usage::Normal))
+        rt.block_on(client.variants_by_stop(50254, Usage::Normal))
             .unwrap();
-        let expected = vec![
-            RouteVariante {
-                key: "1-0".to_string(),
-                name: Some("1 On-Request".to_string()),
-            },
-            RouteVariante {
-                key: "W362-0".to_string(),
-                name: Some("Employee bus".to_string()),
-            },
-            RouteVariante {
-                key: "W322-0".to_string(),
-                name: Some("Employee bus".to_string()),
-            },
-            RouteVariante {
-                key: "W372-0".to_string(),
-                name: Some("Employee bus".to_string()),
-            },
-            RouteVariante {
-                key: "57-0-S".to_string(),
-                name: Some("Southdale Express to Southdale".to_string()),
-            },
-            RouteVariante {
-                key: "19-1-D".to_string(),
-                name: Some("Marion-Logan-Notre Dame to Windsor Park via Drake".to_string()),
-            },
-            RouteVariante {
-                key: "19-1-A".to_string(),
-                name: Some("Marion-Logan-Notre Dame to Windsor Park via Autumnwood".to_string()),
-            },
-            RouteVariante {
-                key: "W332-0".to_string(),
-                name: Some("Employee bus".to_string()),
-            },
-        ];
-
-        //dbg!("{:?},{:?}", &actual, &expected);
-        assert_eq!(actual, expected);
     }
 
     #[test]
@@ -171,41 +133,7 @@ mod test {
         let client = crate::TransitClient::new(
             std::env::var("WPG_TRANSIT_API_KEY").unwrap_or(String::from("")),
         );
-        let actual = rt
-            .block_on(client.variants_by_stops(vec![10652, 10907], Usage::Normal))
+        rt.block_on(client.variants_by_stops(vec![10652, 10907], Usage::Normal))
             .unwrap();
-        let expected = vec![
-            RouteVariante {
-                key: "I304-0".to_string(),
-                name: Some("Fort Rouge Garage".to_string()),
-            },
-            RouteVariante {
-                key: "65-1-D".to_string(),
-                name: Some("Grant Express to Downtown (City Hall)".to_string()),
-            },
-            RouteVariante {
-                key: "S409-0-PM".to_string(),
-                name: Some("to School Charter".to_string()),
-            },
-            RouteVariante {
-                key: "38-0-T".to_string(),
-                name: Some("Salter to Templeton & McPhillips".to_string()),
-            },
-            RouteVariante {
-                key: "S425-0-PM".to_string(),
-                name: Some("to From St.Paul's College (PM)".to_string()),
-            },
-            RouteVariante {
-                key: "38-1-F".to_string(),
-                name: Some("Salter to The Forks".to_string()),
-            },
-            RouteVariante {
-                key: "66-1-D".to_string(),
-                name: Some("Grant to Downtown (City Hall)".to_string()),
-            },
-        ];
-
-        //dbg!("{:?},{:?}", &actual, &expected);
-        assert_eq!(actual, expected);
     }
 }
