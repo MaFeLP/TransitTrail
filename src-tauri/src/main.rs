712300bf84da04b5c2a6b22ded98eb1eb54bebc2
MaFeLP <60669873+MaFeLP@users.noreply.github.com>
2023-06-06 16:11:23 -0500

    📗 Added comments and polished the settings
    Signed-off-by: MaFeLP <60669873+MaFeLP@users.noreply.github.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,7 @@
 mod settings;
 mod service_advisory;
 
-use log::{info, error};
+use log::error;
 use settings::{load_settings, get_settings, save_settings, Settings, test_token, reset_settings};
 use std::fmt::Debug;
 use tauri::async_runtime::Mutex;
@@ -17,17 +17,49 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+/// Get access to the global TransitClient of the application.
+/// 
+/// Just add `client: State<'_, ClientState>` as a parameter to the function
+/// and tauri will automatically give you access to the managed client.
+/// 
+/// Any function that uses the client **has to return a `Result` and be async!**
+/// 
+/// ```
+/// #[tauri::command]
+/// pub async fn client_usage(client: State<'_, ClientState>) -> Result<(), &'static str> {
+///     let client = client.0.lock().await;
+///     // --snip--
+/// }
+/// ```
 pub struct ClientState (
+    /// The actual client
     pub Mutex<TransitClient>,
 );
 
+/// Get access to the global settings of the application.
+/// 
+/// Just add `settings: State<'_, SettingsState>` as a parameter to the function
+/// and tauri will automatically give you access to the managed state.
+/// 
+/// Any function that uses the settings **has to return a `Result` and be async!**
+/// 
+/// ```no_run
+/// use tauri::State;
+/// use crate::SettingsState;
+/// 
+/// #[tauri::command]
+/// pub async fn settings_usage(settings: State<'_, SettingsState>) -> Result<(), ()> {
+///     let settings = settings.0.lock().await;
+///     // --snip--
+/// }
+/// ```
 pub struct SettingsState (
     pub Mutex<Settings>
 );
 
 fn main() {
     let user_settings = load_settings().unwrap_or_default();
-    info!("Loaded user settings: {user_settings:?}");
+    println!("[Before Init]: Loaded user settings: {user_settings:?}");
 
     tauri::Builder::default()
         .manage(ClientState(Mutex::new(TransitClient::new(user_settings.api_key.clone()))))
@@ -49,7 +81,13 @@ fn main() {
         .expect("error while running tauri application");
 }
 
-fn error_string<'a, E: Debug>(error: &E, message: &'a str) -> &'a str {
+/// Format an error nicely and print it out to the console.
+/// 
+/// # Arguments
+///
+/// * error: The error that will be printed with debug format
+/// * message: The message that will be printed before the error. This is also the return value
+pub fn error_string<'a, E: Debug>(error: &E, message: &'a str) -> &'a str {
     error!("{message}: {error:?}");
     message
 }
